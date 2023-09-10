006bcf31f97ecc0452747f285cec9b14fce1e0f2
Preciusmdp <preciusmdp@hotmail.com>
2023-09-10 10:50:19 -0300

    ok

---
+++ Clase 2/prueba.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> Mi primer programa de back</h1>
+    
+    <?php
+
+    $Saludo = "Hola Potrero";
+    $saludoDePotreroDigital = "Hello Rochi";
+    $saludo_fer = "Hola Mi Bichito";
+
+    echo $saludo_fer;
+    echo $Saludo;
+    echo $saludoDePotreroDigital;
+
+
+    ?>
+
+</body>
+
+</html>
